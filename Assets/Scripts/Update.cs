ab2ef71ada86fccf98136744c21c067a0d50d6c3
Raidenxd2 <64755433+Raidenxd2@users.noreply.github.com>
2022-10-12 19:05:49 -0400

    V8 Beta 2
    Added Update System.

---
+++ Assets/Scripts/Update.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Update : MonoBehaviour
+{
+
+    string newVersion;
+    public string currentVersion;
+    public GameObject UpdateScreen;
+
+    public void GotoDownload()
+    {
+        Application.OpenURL("https://raidenxd2.itch.io/cookieclickermp4#download");
+    }
+
+    public void CheckForUpdatesGet()
+    {
+        StartCoroutine(GetRequest("https://itch.io/api/1/x/wharf/latest?target=raidenxd2/cookieclickermp4&channel_name=win-32"));
+    }
+
+    void CheckForUpdates()
+    {
+        Debug.Log("Old Version: " + currentVersion);
+        Debug.Log("New Version: " + newVersion);
+        if (currentVersion != newVersion)
+        {
+            UpdateScreen.SetActive(true);
+        }
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    newVersion = webRequest.downloadHandler.text;
+                    CheckForUpdates();
+                    break;
+            }
+        }
+    }
+}
