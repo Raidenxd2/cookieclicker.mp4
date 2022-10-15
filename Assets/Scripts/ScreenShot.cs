5069cf5e37e8727d3e405c7c15f201f27bb24a8c
Raidenxd2 <64755433+Raidenxd2@users.noreply.github.com>
2022-10-15 13:13:33 -0400

    V8 Beta 3
    Added screenshot feature (press F12)
    Added update feature
    Cookie texture is now white to allow for different colors

---
+++ Assets/Scripts/ScreenShot.cs
@@ -0,0 +1,75 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ using System.IO;
+ 
+public class ScreenShot : MonoBehaviour 
+{
+     
+    public string filePath;
+    public Notification notification;
+
+    void Start()
+    {
+        try
+        {
+            if (!Directory.Exists(Application.dataPath + filePath))
+            {
+                Directory.CreateDirectory(Application.dataPath + filePath);
+            }
+ 
+        }
+        catch (IOException ex)
+        {
+            Debug.LogError(ex.Message);
+            notification.ShowNotification(ex.Message, "Error");
+        }
+    }
+
+    public void DeleteScreenshots()
+    {
+        try
+        {
+            Directory.Delete(Application.dataPath + filePath, true);
+            Directory.CreateDirectory(Application.dataPath + filePath);
+        }
+        catch (IOException ex)
+        {
+            Debug.LogError(ex.Message);
+            notification.ShowNotification(ex.Message, "Error");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F12))
+        {
+            TakeScreenshot();
+        }
+    }
+
+    public void TakeScreenshot()
+    {
+        //string fileName = "screenshot" + Random.Range(0, 500000) + ".png";
+        string datetime = System.DateTime.Now.ToString("MM-dd-yyyy hh;mm;ss");
+        ScreenCapture.CaptureScreenshot(Application.dataPath + "/screenshots/" + datetime + ".png", 2);
+        if (File.Exists(Application.dataPath + "/screenshots/" + datetime))
+        {
+            ScreenshotTaken();
+        }
+        notification.ShowNotification("Screenshot saved at " + Application.dataPath + filePath + "/" + datetime + ".png", "Screenshot Taken");
+    }
+
+    void ScreenshotTaken()
+    {
+        Debug.Log("File Already Taken!");
+        //string fileName = "screenshot" + Random.Range(0, 500000) + ".png";
+        string datetime = System.DateTime.Now.ToString("MM-dd-yyyy hh;mm;ss");
+        ScreenCapture.CaptureScreenshot(Application.dataPath + "/screenshots/" + datetime + ".png", 2);
+        if (File.Exists(Application.dataPath + "/screenshots/" + datetime + ".png"))
+        {
+            ScreenshotTaken();
+        }
+        notification.ShowNotification("Screenshot saved at " + Application.dataPath + filePath + "/" + datetime + ".png", "Screenshot Taken");
+    }
+     
+}
